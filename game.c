7fd2ead2fea614ae33fa53b50c499f4606889596
lem-ma <36986550+lem-ma@users.noreply.github.com>
2018-10-18 21:39:08 +0800

    Update game.c

--- game.c
+++ game.c
@@ -6,21 +6,21 @@
 
 int main(int argc, char *argv[])
 {
-    int m;
+    int m,mod;
+    if(argc==1||(mod=atoi(argv[1]))>31||mod<3) mod=13;
     char ind,c=0,pa[4],answer[8],hum[20];
-    clock_t start;
-    double dt;
     srand(time(NULL));
     while(c!=EOF)
     {
-        do for(int i=0;i<4;i++) pa[i]=(rand()%13)+1;
-        while(!(m=tryout(answer, pa)));
-        printf("Puzzle: %hhd, %hhd, %hhd, %hhd\nPress <Enter> to"
+        fputs("Initializing...",stdout);
+        do for(int i=0;i<4;i++) pa[i]=(rand()%mod)+1;
+        while(!(m=solve24_tryout(answer, pa)));
+        printf("\rPuzzle: %hhd, %hhd, %hhd, %hhd\nPress <Enter> to"
                 " reveal the answer.",pa[0],pa[1],pa[2],pa[3]);
-        start=clock();
+        clock_t start=clock();
         while((c=getchar())!='\n'&&c!='\r'&&c!=EOF);
-        dt=(double)(clock()-start)/CLOCKS_PER_SEC;
-        puts(output(hum,answer,m));
+        double dt=(double)(clock()-start)/CLOCKS_PER_SEC;
+        puts(solve24_output(hum,answer,m));
         printf("Time used: %.3lf seconds.\nPress <Enter> to start a new"
                 " puzzle, or <Ctrl-Z><Enter> to end the game.\n",dt);
         while((c=getchar())!='\n'&&c!='\r'&&c!=EOF);
